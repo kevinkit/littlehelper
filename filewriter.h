dc36eb1a3c9252a217bffe2c8744d7dea22aba76
Kevin Alexander Höfle <kevinahoefle@gmail.com>
2014-06-17 11:18:20 -0300

    Update filewriter.h
    Added some new fuctions:
    0. size_bytes gives you the information about the size of a file in bytes
    1.openall opens a file and reads out all values into an array
    2.openfrac opens a file and reads out a fraction of it. You have to specify the length of the 
     fraction and the index (e.g fraction = 100 ; index = 2   will read the data from 100 to 200.)
    3. splitarray can be used to split an array into smaller arrays.

--- filewriter.h
+++ filewriter.h
@@ -1,17 +1,25 @@
 #ifndef FILEWRITER_H
 #define FILEWRITER_H
 
+#include <sys/stat.h>
+#include <sys/types.h>
 
+//ONLY WORKS FOR LINUX !
 
 //////////////////////////////////////////////////////////////////////////////////
-/* all "add____" functions are adding values to the file			*/
-/* all "over___" functions are overwriting the values in the file ! 		*/
+/* all "add____" functions are adding values to the file                        */
+/* all "over___" functions are overwriting the values in the file !             */
+/* openall opens a file and reads out the complete file                         */
+/* openfrac opens a file and reads a specific amount of values out              */
 /*______________________________________________________________________________*/
 /* all functions besides addo or overo are not good for loops due to opening    */
 /* and closing the file everytime they are used. Better use addo or overo       */
-/* for opening them only once. 							*/
-/*										*/
+/* for opening them only once.                                                  */
+/*                                                                              */
 /////////////////////////////////////////////////////////////////////////////////
+
+
+
 
 FILE* addo(FILE* file, char* filename)
 {
@@ -171,9 +179,80 @@ int overowcc(char* filename, char* string)
 }
 
 
+int size_bytes(const char* path)
+{
+        struct stat attribute;
+        stat(path, &attribute);
+
+        return  (int) attribute.st_size;
+}
 
 
+int openall(const char* path, int* values)
+{
+        int i = 0;
+        struct stat attribute;
+        stat(path, &attribute);
 
+        FILE *file;
+        file = fopen(path, "r");
+
+        printf("%d\n", (int) attribute.st_size);
+        if(file == NULL)
+        {
+                printf("Error opening file");
+                return -1;
+        }
+                for(; i < (int) (attribute.st_size)/8; i++)
+                {
+                        fscanf(file, "%d", &values[i]);
+                        //printf("no segmentation fault \n");
+                }
+
+        fclose(file);
+}
+
+int openfrac(const char* path, int* values, int fraction, int index)
+{
+        printf("Reading out data will be from %d to %d", index*fraction, (index*fraction) + fraction);
+        int i =0;
+        struct stat attribute;
+        stat(path, &attribute);
+
+        FILE *file;
+        file  = fopen(path, "r");
+        if(attribute.st_size < ((index*fraction) + fraction))
+        {
+                printf("Index is out of bonds in openfrac");
+                return -1;
+        }
+
+        int* values_puffer = (int*) malloc((attribute.st_size +1));
+        if(file == NULL)
+        {
+                printf("Error opening file \n");
+                return -1;
+        }
+
+        for(; i < (int) (attribute.st_size / 8); i++)
+        {
+                fscanf(file, "%d", &values_puffer[i]);
+                printf("no segment fault %i \n", i);
+        }
+
+        splitarray(values_puffer, fraction, index, values);
+        fclose(file);
+
+}
+
+int splitarray(int *array, int fraction, int index, int* value)
+{
+        int i = 0;
+        for(; i < fraction; i++)
+        {
+                value[i] = array[i+(fraction*index)];
+        }
+}
 
 
 
